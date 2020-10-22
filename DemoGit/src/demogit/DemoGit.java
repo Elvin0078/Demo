dfb8ba9422ddb3c7dfc6cc210f5de0c93922ccc2
workman <workman@workman-PC>
2020-10-22 21:59:58 +0400

    Test 3

--- DemoGit/src/demogit/DemoGit.java
+++ DemoGit/src/demogit/DemoGit.java
@@ -3,7 +3,7 @@ package demogit;
 public class DemoGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git 2");
     }
 
 }
